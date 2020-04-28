b721ef3ec8bf4384457a412cbbb66c69c926749e
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2020-04-28 21:54:07 +0300

    started

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+console.log(user);
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = 'false';
+console.log(user);
+
+const keys = Object.keys(user);
+
+for (const key of keys) {
+  console.log(user[key]);
+}
