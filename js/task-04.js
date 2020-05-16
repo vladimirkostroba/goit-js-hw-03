47f8462234f6da4b0565850d459388a4e089dfba
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2020-05-16 21:33:40 +0300

    end

---
+++ js/task-04.js
@@ -0,0 +1,27 @@
+const countTotalSalary = function(employees) {
+  let total = 0;
+
+  Object.values(employees).forEach(salary => {
+    total += salary;
+  });
+
+  return total;
+};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
